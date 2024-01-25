8c2e715977eadc140c502246b0e9894e3ff8adb4
hansin91 <hansinsusatya@gmail.com>
2024-01-25 14:03:47 +0700

    finishing helper libraries and setup github actions

---
+++ src/error-handler.ts
@@ -0,0 +1,86 @@
+import { StatusCodes } from 'http-status-codes'
+
+export interface IErrorResponse {
+  message: string
+  statusCode: number
+  status: string
+  service: string
+  serializeErrors(): IError
+}
+
+export interface IError {
+  message: string
+  statusCode: number
+  status: string
+  service: string
+}
+
+export abstract class CustomError extends Error {
+  abstract statusCode: number
+  abstract status: string
+  service: string
+
+  constructor(message: string, service: string) {
+    super(message)
+    this.service = service
+  }
+
+  serializeErrors(): IError {
+    return {
+      message: this.message,
+      statusCode: this.statusCode,
+      status: this.status,
+      service: this.service
+    }
+  }
+}
+
+export class BadRequestError extends CustomError {
+  statusCode = StatusCodes.BAD_REQUEST
+  status = 'error'
+  constructor(message: string, service: string) {
+    super(message, service)
+  }
+}
+
+export class NotFoundError extends CustomError {
+  statusCode = StatusCodes.NOT_FOUND
+  status = 'error'
+  constructor(message: string, service: string) {
+    super(message, service)
+  }
+}
+
+export class NotAuthorizedError extends CustomError {
+  statusCode = StatusCodes.UNAUTHORIZED
+  status = 'error'
+  constructor(message: string, service: string) {
+    super(message, service)
+  }
+}
+
+export class FileTooLargeError extends CustomError {
+  statusCode = StatusCodes.REQUEST_TOO_LONG
+  status = 'error'
+  constructor(message: string, service: string) {
+    super(message, service)
+  }
+}
+
+export class ServerError extends CustomError {
+  statusCode = StatusCodes.SERVICE_UNAVAILABLE
+  status = 'error'
+  constructor(message: string, service: string) {
+    super(message, service)
+  }
+}
+
+export interface ErrnoException extends Error {
+  errno?: number
+  code?: string
+  path?: string
+  syscall?: string
+  stack?: string
+}
+
+
---
+++ src/index.ts
@@ -0,0 +1,67 @@
+export {
+  IAuthPayload,
+  IAuthDocument,
+  IAuthBuyerMessageDetails,
+  IEmailMessageDetails,
+  IAuth,
+  ISignUpPayload,
+  ISignInPayload,
+  IForgotPassword,
+  IResetPassword,
+  IReduxAuthPayload,
+  IReduxAddAuthUser,
+  IAuthResponse,
+  IAuthUser,
+  IReduxLogout
+} from './interfaces/auth.interface'
+export { IBuyerDocument, IReduxBuyer } from './interfaces/buyer.interface'
+export {
+  IChatBoxProps,
+  IChatBuyerProps,
+  IChatMessageProps,
+  IChatSellerProps,
+  IConversationDocument,
+  IMessageDetails,
+  IMessageDocument,
+} from './interfaces/chat.interface'
+export { IEmailLocals } from './interfaces/email.interface'
+export {
+  GigType,
+  ICreateGig,
+  IGigCardItems,
+  IGigContext,
+  IGigInfo,
+  IGigTopProps,
+  IGigViewReviewsProps,
+  IGigsProps,
+  ISelectedBudget,
+  ISellerGig
+} from './interfaces/gig.interface'
+export {
+  IDeliveredWork,
+  IExtendedDelivery,
+  IOffer,
+  IOrderDocument,
+  IOrderEvents,
+  IOrderMessage,
+  IOrderNotifcation,
+  IOrderReview
+} from './interfaces/order.interface'
+export { IRatingCategories, IRatingCategoryItem, IRatingTypes, IReviewDocument, IReviewMessageDetails } from './interfaces/review.interface'
+export { IHitsTotal, IPaginateProps, IQueryList, IQueryString, ISearchResult, ITerm } from './interfaces/search.interface'
+export { SellerType, ICertificate, IEducation, IExperience, ILanguage, ISellerDocument } from './interfaces/seller.interface'
+export { uploadFile, uploadVideo } from './cloudinary-upload'
+export {
+  IError,
+  IErrorResponse,
+  CustomError,
+  BadRequestError,
+  NotAuthorizedError,
+  NotFoundError,
+  FileTooLargeError,
+  ServerError,
+  ErrnoException
+} from './error-handler'
+export { verifyGatewayRequest } from './gateway-middleware'
+export { winstonLogger } from './logger'
+export { firstLetterUppercase, lowerCase, toUpperCase, isEmail, isDataURL } from './helpers'
