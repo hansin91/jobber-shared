32dc413ed0c661914068f9eeedeb1992d671d7f9
hansin91 <hansinsusatya@gmail.com>
2024-02-01 15:21:45 +0700

    gatewaytoken

--- src/gateway-middleware.ts
+++ src/gateway-middleware.ts
@@ -5,10 +5,10 @@ import { NotAuthorizedError } from './error-handler'
 const tokens: string[] = ['auth', 'seller', 'gig', 'search', 'buyer', 'chat', 'order', 'review']
 
 export function verifyGatewayRequest(req: Request, res: Response, next: NextFunction): void {
-  if (!req.headers?.gatewayToken) {
+  if (!req.headers?.gatewaytoken) {
     throw new NotAuthorizedError('Invalid request', 'verifyGatewayRequest() method: Request not coming from api gateway')
   }
-  const token: string = req.headers?.gatewayToken as string
+  const token: string = req.headers?.gatewaytoken as string
   if (!token) {
     throw new NotAuthorizedError('Invalid request', 'verifyGatewayRequest() method: Request not coming from api gateway')
   }
