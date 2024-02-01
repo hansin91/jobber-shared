44dc6b4976441f62180a80bae4033c7c5baa85b8
hansin91 <hansinsusatya@gmail.com>
2024-02-01 10:10:08 +0700

    edit package.json and gateway middleware

--- src/gateway-middleware.ts
+++ src/gateway-middleware.ts
@@ -1,5 +1,5 @@
 import JWT from 'jsonwebtoken'
-import { Request, Response, NextFunction, request } from 'express'
+import { Request, Response, NextFunction } from 'express'
 import { NotAuthorizedError } from './error-handler'
 
 const tokens: string[] = ['auth', 'seller', 'gig', 'search', 'buyer', 'chat', 'order', 'review']
@@ -14,11 +14,12 @@ export function verifyGatewayRequest(req: Request, res: Response, next: NextFunc
   }
 
   try {
-    const payload: { id: string, iat: number } = JWT.verify(token, '') as { id: string, iat: number }
+    const payload: { id: string, iat: number } = JWT.verify(token, '25570c313ce5e2dc985a5543d992ab97') as { id: string, iat: number }
     if (!tokens.includes(payload.id)) {
       throw new NotAuthorizedError('Invalid request', 'verifyGatewayRequest() method: Request payload is invalid')
     }
   } catch (error) {
     throw new NotAuthorizedError('Invalid request', 'verifyGatewayRequest() method: Request not coming from api gateway')
   }
+  next()
 }
